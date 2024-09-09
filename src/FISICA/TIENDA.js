bb93fb89abbc967673261ff218c102d19af0a2a0
Diego <duvierdiegodavid.0715@gmail.com>
2024-09-08 20:14:34 -0500

    PREVIA PAGINA

--- src/FISICA/TIENDA.js
+++ src/FISICA/TIENDA.js
@@ -1,3 +1,15 @@
 /*
 *MAGIAAA
 */
+
+const header = document.querySelector("#encabezado");
+const contenedor = document.querySelector("#contenedor")
+const bodey = document.querySelector("#body")
+
+window.addEventListener("scroll", function(){
+    if (contenedor.getBoundingClientRect().top < 10){
+        header.classList.add("scroll")
+    } else {
+        header.classList.remove("scroll")
+    }
+})
